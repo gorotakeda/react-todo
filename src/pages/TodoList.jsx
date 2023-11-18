344ff0c34dd67562b2580fae6bc6a9f3ac996d78
gorotakeda <rugbytkg3@gmail.com>
2023-11-19 01:03:17 +0900

    components分割

---
+++ src/pages/TodoList.jsx
@@ -0,0 +1,14 @@
+import{Todo} from '../components/todo'
+export const TodoList = () => {
+  return(
+    <div>
+      <h1>TodoList</h1>
+      <Todo />
+      <Todo />
+      <Todo />
+      <Todo />
+      <Todo />
+      <Todo />
+    </div>
+  );
+}
