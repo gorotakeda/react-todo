5baef66eee5bcd80234f29a9a9bf9330aa0b40bb
gorotakeda <rugbytkg3@gmail.com>
2023-11-19 02:12:34 +0900

    TODOリスト

--- src/components/todo.jsx
+++ src/components/todo.jsx
@@ -1,14 +1,22 @@
-import { useState } from "react"
+import React from "react"
 
-export const Todo = ({}) => {
-  const [todo, setTodo] = useState('筋トレ')
+export const Todo = ({todoItem, setToggleDoneById, setTitleById}) => {
+
   return (
     <>
       <div>Todo</div>
-      <input type="text" value={todo}
-          onChange={(e) => {
-            setTodo(e.target.value)
-          }}
+      <p>{todoItem.id}</p>
+      <div>
+        <p>
+          {todoItem.done ? '完了' : '未完了'}
+        </p>
+        <input type="checkbox" checked={todoItem.done} onChange={() => setToggleDoneById(todoItem.id)} />
+      </div>
+      <input type="text" value={todoItem.title}
+          onChange={(event) => {
+            setTitleById(todoItem.id, event.target.value)
+          }
+        }
       />
     </>
   )
--- src/pages/TodoList.jsx
+++ src/pages/TodoList.jsx
@@ -1,14 +1,63 @@
+import { useState } from 'react';
 import{Todo} from '../components/todo'
+
+
 export const TodoList = () => {
+
+  const [todoList, setTodoList] = useState([
+    { id: 1, title: "筋トレ", done: false},
+    { id: 2, title: "勉強" ,done: false},
+    { id: 3, title: "犬の散歩", done: false},
+    { id: 4, title: "洗濯", done: false},
+    { id: 5, title: "料理", done: false},
+  ])
+
+  const setToggleDoneById = (todoId) => {
+    setTodoList(
+      todoList.map((todo) => {
+        if (todo.id === todoId) {
+          return {
+            ...todo,
+            done: !todo.done
+          }
+        } else {
+          return todo
+        }
+      })
+    )
+  }
+
+  const setTitleById = (todoId, title) => {
+    setTodoList(
+      todoList.map((todo) => {
+        if (todo.id === todoId) {
+          return {
+            ...todo,
+            title
+          }
+        } else {
+          return todo
+        }
+      })
+    )
+  }
+
+  const todoDoneCount = todoList.filter((todo) => todo.done).length
+
   return(
+
     <div>
+      <p>TODO達成個数 {todoDoneCount} / TODO数 { todoList.length }</p>
+
+      {todoDoneCount === todoList.length && <p>全てのTODOを達成しました。</p>}
+
       <h1>TodoList</h1>
-      <Todo />
-      <Todo />
-      <Todo />
-      <Todo />
-      <Todo />
-      <Todo />
+      {
+        todoList.map((todo) => {
+          return <Todo key={todo.id} todoItem={todo} setToggleDoneById={setToggleDoneById} setTitleById={setTitleById}/>
+        })
+      }
+
     </div>
   );
 }
